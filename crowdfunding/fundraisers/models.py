4e42ac90de5cd1fd7a763e99e3200f31b83056d3
mariabodriv <mariabodero94@gmail.com>
2025-11-18 19:35:00 +1000

    wrote model relations

--- crowdfunding/fundraisers/models.py
+++ crowdfunding/fundraisers/models.py
@@ -9,3 +9,13 @@ class Fundraiser(models.Model):
     image= models.URLField()
     is_open=models.BooleanField()
     data_created=models.DateTimeField(auto_now_add=True) #we are saying here that the date will automatically be set to the current date when a new record is created.
+
+class Pledge(models.Model):
+    amount= models.IntegerField()
+    comment= models.CharField(max_length=200)
+    anonymous = models.BooleanField()
+    #This tells Django that each Pledge needs to have the ID of a Fundraiser saved in this field. The on_delete functions says, that if a Fundraiser is deleted or doesn't exist, we will also delete the pledge linked to that Fundraiser.
+    fundraiser= models.ForeignKey('Fundraiser',
+        on_delete=models.CASCADE,
+        related_name='pledges'         
+    )
